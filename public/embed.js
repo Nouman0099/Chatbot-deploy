7f4dd1217d942e517a5062ba3ae5b45d590e53b0
Noumanali0099 <nouman@tekvix.com>
2025-04-18 12:09:11 +0500

    add

--- public/embed.js
+++ public/embed.js
@@ -4,11 +4,12 @@ function createChatWidget(options) {
     iframe.src = `${options.apiUrl}?welcomeMessage=${encodeURIComponent(options.welcomeMessage || '')}`;
     iframe.style.position = 'fixed';
     iframe.style.bottom = '24px';
-    iframe.style[options.position || 'right'] = '20px';
+    iframe.style[options.position || 'center'] = '20px';
     iframe.style.width = '550px';
     iframe.style.height = '550px';
     iframe.style.border = 'none';
     iframe.style.borderRadius = '10px';
+    iframe.style.padding = '10px'
     iframe.style.boxShadow = '0 4px 12px rgba(0, 0, 0, 0.15)';
     iframe.style.backgroundColor = 'transparent'; // Ensure transparency
     iframe.setAttribute('id', 'custom-chatbot');
