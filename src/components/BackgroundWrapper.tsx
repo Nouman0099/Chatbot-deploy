f2fdb58bbff12f1d287e10819ddcfb630684ef61
Noumanali0099 <nouman@tekvix.com>
2025-04-21 12:59:37 +0500

    embed.js

--- src/components/BackgroundWrapper.tsx
+++ src/components/BackgroundWrapper.tsx
@@ -1,15 +1,33 @@
+// "use client";
+
+// import { usePathname } from "next/navigation";
+// import { ReactNode } from "react";
+
+// export default function BackgroundWrapper({ children }: { children: ReactNode }) {
+//   const pathname = usePathname();
+
+//   const isEmbedRoute =
+//     pathname === "/popup/financeforfounder" || pathname === "/financeforfounder";
+
+//   return (
+//     <div className={isEmbedRoute ? "bg-transparent" : "bg-indigo-950"}>{children}</div>
+//   );
+// }
+
 "use client";
 
-import { usePathname } from "next/navigation";
+import { useSearchParams } from "next/navigation";
 import { ReactNode } from "react";
 
 export default function BackgroundWrapper({ children }: { children: ReactNode }) {
-  const pathname = usePathname();
+//   const pathname = usePathname();
+  const searchParams = useSearchParams();
 
-  const isEmbedRoute =
-    pathname === "/popup/financeforfounder" || pathname === "/financeforfounder";
+  const isEmbedded = searchParams.get("embed") === "true";
 
   return (
-    <div className={isEmbedRoute ? "bg-transparent" : "bg-indigo-950"}>{children}</div>
+    <div className={isEmbedded ? "bg-transparent" : "bg-indigo-950"}>
+      {children}
+    </div>
   );
 }
